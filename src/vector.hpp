620be0db21e3fbbbc7e2a5751e88006516ee3bc3
Aarti Chaudhary <aartic88@gmail.com>
2023-11-20 23:20:58 +0530

    Vector basic working version

--- src/vector.hpp
+++ src/vector.hpp
@@ -7,25 +7,39 @@ We want to make a dynamic vector class
 Array(10)  => Allocate the array of 10 element in heap, When this object gets deleted
 It will automatically delete all the stuff
 */
-#include <utility>
-#include <cstdint>
 #include <boost/log/trivial.hpp>
+#include <cstdint>
+#include <utility>
 #include "util-logger.hpp"
 // use preprocesor #define
 #define DEFAULT_CAPACITY 16
-class Vector{
-   T* data;
+template <typename T>
+class Vector {
   uint32_t length;
   uint32_t capacity;
+  T* data;
 
-  public:
+ public:
   /**
    * length = 0
    * Capacity  = default or passed as parameter
    * 
   */
-  Vector(uint32_t capacity = DEFAULT_CAPACITY):length(0){
-
+  Vector(uint32_t capacity = DEFAULT_CAPACITY)
+      : length(0),
+        capacity(capacity),
+        data(reinterpret_cast<T*>(malloc(sizeof(T) * capacity))) {}
+  ~Vector() {
+    for (uint32_t i = 0; i < length; i++) {
+      data[i].~T();
+    }
+    free(data);
   }
+  void pushBack(T obj) {
+    new (data + length) T(obj);
+    ++length;
+    //TODO:: If length = capacity
+  }
+  T& operator[](uint32_t index) { return data[index]; }
 };
 #endif
--- tests/vector/vector-tests.cpp
+++ tests/vector/vector-tests.cpp
@@ -5,8 +5,8 @@
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <catch2/catch_all.hpp>
 
-#include "../../src/vector.hpp"
 #include "../../src/util-logger.hpp"
+#include "../../src/vector.hpp"
 
 namespace attrs = boost::log::attributes;
 int main(int argc, char* argv[]) {
@@ -15,9 +15,51 @@ int main(int argc, char* argv[]) {
   return result;
 }
 
+class A {  // NOLINT
+  int* i;
+
+ public:
+  explicit A(int i) : i(new int(i)) {
+    LOG(info) << "Constuctor of A is called " << this->i << " " << *(this->i);
+  }
+  A(const A& rhs) : i(new int(*(rhs.i))) {
+    LOG(info) << "Copy Constuctor of A is called from " << this->i << " "
+              << *(this->i);
+  }
+
+  A& operator=(const A& rhs) {
+    A tempCopy(rhs);
+    std::swap(this->i, tempCopy.i);
+    LOG(info) << "Copy Assignment of A is called from " << this->i << " "
+              << *(this->i);
+    return *this;
+  }
+
+  bool operator==(const A& rhs) const { return *i == *(rhs.i); }
+  ~A() {
+    LOG(info) << "Destructor of A is called for " << this->i << " "
+              << *(this->i);
+    delete i;
+  }
+};
+
 TEST_CASE("vector creation-destructor should work") {
-  BOOST_LOG_NAMED_SCOPE("vector-create-test");
-  
-  Vector<int> a;
-  Vector<A> a;  // Does not support default constructor 
+  BOOST_LOG_NAMED_SCOPE("vector-pushback-test");
+
+  Vector<A> a;  // Does not support default constructor
+  a.pushBack(A(1));
+  a.pushBack(A(2));
+  LOG(info) << "Comparing pushbacks now ";
+  CHECK(a[0] == A(1));
+  CHECK(a[1] == A(2));
+}
+
+TEST_CASE("vector-index assignment-operator should work") {
+  BOOST_LOG_NAMED_SCOPE("vector-index-assignment-test");
+
+  Vector<A> a;  // Does not support default constructor
+  a.pushBack(A(1));
+  a[0] = A(2);
+  LOG(info) << "Comparing assignment now ";
+  CHECK(a[0] == A(2));
 }
